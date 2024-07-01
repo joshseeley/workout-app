7b9bf377f504c9b1624c182a60661538fe30c34a
Josh Seeley <joshseeley@gmail.com>
2024-07-01 15:51:45 -0300

    update WorkoutForm

--- src/components/workoutForm.js
+++ src/components/workoutForm.js
@@ -2,18 +2,61 @@ import { useState } from "react"
 
 const WorkoutForm = () => {
     const [title, setTitle] = useState('')
-    const [load, setLoade] = useState('')
+    const [load, setLoad] = useState('')
     const [reps, setReps] = useState('')
+    const [error, setError] = useState(null);
+    
+    const haldleSubmit = async (e) => {
+        e.preventDefault()
+
+        const workout = {title, load, reps};
+
+        const response = await fetch('/api/workouts', {
+            method: 'POST',
+            body: JSON.stringify(workout),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const json = await response.json() 
+
+        if(!response.ok) {
+            setError(json.error)
+        }
+        if (response.ok) {
+            setTitle('')
+            setLoad('')
+            setReps('')
+            setError(null)
+            console.log('New workout created', json)
+        }
+    }
+        
     return (
-        <form className="create">
+        <form className="create" onSubmit={handleSubmit}>
             <h3>Add a new workout</h3>
             <label >Exercise Title:</label>
             <input 
                 type="text"
                 onChange={(e) => setTitle(e.target.value)}
                 value={title}
-                />
+            />
+            <label >Load in kgs:</label>
+            <input 
+                type="number"
+                onChange={(e) => setLoad(e.target.value)}
+                value={load}
+            />
+            <label >Number of reps:</label>
+            <input 
+                type="number"
+                onChange={(e) => setReps(e.target.value)}
+                value={reps}
+            />
+            <button>Add Workout</button>
         </form>
 
     )
 }
+
+export default WorkoutForm
